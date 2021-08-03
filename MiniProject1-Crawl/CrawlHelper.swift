fded06e96237c8803902827d1bfd6a91998d55c2
Jamie <jamie.yjchen@gmail.com>
2021-08-03 07:41:46 -0700

    Finish Mini Project1

---
+++ MiniProject1-Crawl/CrawlHelper.swift
@@ -0,0 +1,62 @@
+//
+//  CrawlHelper.swift
+//  MiniProject1-Crawl
+//
+//  Created by Jamie Chen on 2021-08-03.
+//
+
+import Foundation
+
+func isFileDirectory(atPath path: String, name: String) -> FileModel {
+    let fileManager = FileManager.default
+    
+    var isDir: ObjCBool = false
+    
+    return FileModel(path: path, name: name, isExist: fileManager.fileExists(atPath: path+"/\(name)", isDirectory: &isDir), isDirectory: isDir.boolValue)
+}
+
+func getContentOfDirectory(at url: String) {
+    let fileManager = FileManager.default
+    
+    do {
+        let items = try fileManager.contentsOfDirectory(atPath: url)
+        for item in items {
+//            print("Found Item:", item)
+//            if let isDirectory = isFileDirectory(atPath: url, name: item) {
+//                if isDirectory {
+//                    getContentOfDirectory(at: url+item)
+//                } else {
+//                    if item == items.last {
+//                        print(FileHeaders.lastFileHeader.rawValue+item)
+//                    } else {
+//                        print(FileHeaders.fileContentHeader.rawValue+item)
+//                    }
+//                }
+//            }
+            let model = isFileDirectory(atPath: url, name: item)
+            if model.isExist && model.isDirectory {
+                getContentOfDirectory(at: url+"/\(item)")
+            } else if model.isExist && !model.isDirectory {
+                if item == items.last {
+                    print("│ \(FileHeaders.lastFileHeader.rawValue+item)")
+                } else {
+                    print(FileHeaders.fileContentHeader.rawValue+item)
+                }
+            }
+        }
+    } catch {
+        print("there are no items")
+    }
+}
+
+enum FileHeaders: String {
+    case fileContentHeader = "├─"
+    case lastFileHeader = "└─"
+}
+
+struct FileModel {
+    let path: String
+    let name: String
+    let isExist: Bool
+    let isDirectory: Bool
+}
---
+++ MiniProject1-Crawl/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  MiniProject1-Crawl
+//
+//  Created by Jamie Chen on 2021-07-30.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+let path = Bundle.main.resourcePath!
+
+getContentOfDirectory(at: path)
