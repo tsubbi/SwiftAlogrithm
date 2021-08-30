574c17bdf54994df81c24120cd55064caf822f56
Jamie <jamie.yjchen@gmail.com>
2021-08-30 10:44:29 -0700

    diameter

--- TreeBasic/Diameter.swift
+++ TreeBasic/Diameter.swift
@@ -7,18 +7,34 @@
 
 import Foundation
 
-func diameter {
+func diameter() {
+    
     struct Node {
-        let currentNode: Node
-        let nextNode: Node?
+        let currentNode: Int
         let distance: Int
         var visited: Bool
     }
     
+    var nodeList: [Int: [Node]] = [:]
     let n = Int(readLine()!)!
-    for _ in 1...n {
-        let line = readLine()!.split(separator: " ").map({ Int($0)! })
-    }
+    var cnt = 0
+    
+    repeat {
+        let line = readLine()!.split(separator: " ").map {
+            return Int($0) ?? -100
+        }
+        let currentNode = line.first!
+        nodeList[currentNode] = []
+        
+        for i in stride(from: 1, to: line.count, by: 2) {
+            guard line[i] > -1 else { continue }
+            let nextNodeValue = line[i+1]
+            let distance = line[i+2]
+            let newNode = Node(currentNode: nextNodeValue, distance: distance, visited: false)
+            nodeList[currentNode]?.append(newNode)
+        }
+        cnt += 1
+    } while cnt < n
+    
+    
 }
-
-
